43adf77d6fa04a86d2a279043a482521ac8fa0a0
GabrielBTonelli <tonelli.gabriel@gmail.com>
2025-07-29 20:54:12 +0200

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('gabriel.teste@teste.com')
+        cy.get('#password').type('gbtteste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel.teste (não é gabriel.teste? Sair)')
+    })
+})
