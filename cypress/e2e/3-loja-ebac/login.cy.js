ff788209b38719fb85aa6800addf2cd3892b7cde
GabrielBTonelli <tonelli.gabriel@gmail.com>
2025-07-31 20:37:37 +0200

    Massa de dados e Fixture

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     // afterEach(() => {
@@ -33,4 +34,23 @@ describe('Funcionalidade: Login', () => {
         
         cy.get('.woocommerce-error > li').should('exist')
     });
+
+    it('Deve fazer login com sucesso - usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel.teste (não é gabriel.teste? Sair)')
+    });
+
+    it.only('Deve fazer login com sucesso - usando fixture', () => {
+        cy.fixture('perfil').then( info => {
+            cy.get('#username').type(info.usuario)
+            cy.get('#password').type(info.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel.teste (não é gabriel.teste? Sair)')
+        })
+    });
+
 })
