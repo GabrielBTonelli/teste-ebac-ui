19e6f9d88faec2f9954b24ba643f1923173703c7
GabrielBTonelli <tonelli.gabriel@gmail.com>
2025-07-31 22:09:22 +0200

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,30 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos')
+        // cy.visit('produtos')
+        produtosPage.visitarUrl('')
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            // .first()
-            // .last()
-            // .eq('3')
-            .contains('Argus All-Weather Tank')
-            .click()
-
+        produtosPage.buscarProdutoLista('Ariel Roll Sleeve Sweatshirt')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Apollo Running Short'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+
+    });
+
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 })
