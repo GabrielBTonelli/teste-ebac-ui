f8b4774fd45a37ed9753d21d9d6aed51fc2e373f
GabrielBTonelli <tonelli.gabriel@gmail.com>
2025-08-04 19:11:54 +0200

    massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,7 +13,7 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Apollo Running Short'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
@@ -21,10 +21,30 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Abominable Hoodie')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 3
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('L', 'Blue', qtd)
+        // cy.get('.stock').should('contain', 'Fora de estoque')
+        cy.get('.woocommerce-message').should('contain', qtd + ` × “Abominable Hoodie” foram adicionados no seu carrinho.`)
+    });
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho, 
+            dados[0].cor, 
+            dados[0].quantidade)
         
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+        })
+        
+        
+
     });
 })
